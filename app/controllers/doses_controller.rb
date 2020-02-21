e7c385b40ed8af021bc7ea084f12f4815ede3957
Iphona-Clemence Paiement <iphona.paiement@hotmail.com>
2020-02-21 12:19:09 -0500

    Built app features

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,25 +1,29 @@
 class DosesController < ApplicationController
   def new
     @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
   end
 
   def create
     @dose = Dose.new(dose_params)
-    @dose.cocktail_id = params[:cocktail_id]
-    @cocktail = @dose.cocktail
-    @dose.save
-    redirect_to cocktail_path(@cocktail)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render "cocktails/show"
+    end
   end
 
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
-    redirect_to cocktail_path(@cocktail)
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
 
   def dose_params
-    params.require(:dose).permit(:ingredient_id, :description, :cocktail_id)
+    params.require(:dose).permit(:ingredient_id, :description)
   end
 end
